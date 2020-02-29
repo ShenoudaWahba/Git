0f9710f49e6edea83e5a325cf7c624ff9697681c
Shenouda2256 <58535631+Shenouda2256@users.noreply.github.com>
2020-02-29 11:35:24 -0500

    update logout1.java

--- src/Test/logout1.java
+++ src/Test/logout1.java
@@ -2,7 +2,9 @@ package Test;
 
 public class logout1 {
     public static void main(String[] args) {
-        System.out.println("Shenouda");
+       // System.out.println("Shenouda");
         System.out.println("koko");
+        //hello world
     }
+
 }
